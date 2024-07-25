d6d94fa40326efa25af4da7648b0e5d7498d4229
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 15:57:18 +0530

    Update update_custom_ini.sh

--- Terraform/update_custom_ini.sh
+++ Terraform/update_custom_ini.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Create or overwrite the custom.ini file with the master section
+echo "[master]" > custom.ini
+echo "$1 ansible_ssh_private_key_file=/home/vengalasanjay000/gcp_ssh ansible_user=vengalasanjay000" >> custom.ini
+
+# Create or append the nodes section
+echo "[nodes]" >> custom.ini
+shift  # Remove the first argument (master IP)
+for node in "$@"; do
+  echo "$node ansible_ssh_private_key_file=/home/vengalasanjay000/gcp_ssh ansible_user=vengalasanjay000" >> custom.ini
+done
