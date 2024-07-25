c06fcf510294d85a5024e217a4cba1408b1021c8
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 13:40:24 +0530

    Update update_custom_ini.sh

--- Terraform/update_custom_ini.sh
+++ Terraform/update_custom_ini.sh
@@ -3,13 +3,16 @@
 # Define the output file path
 OUTPUT_FILE="/home/runner/work/Main_Project/Main_Project/Terraform/custom.ini"
 
+# Get IP addresses from Terraform outputs
+MASTER_IP="$1"
+NODE_1_IP="$2"
+NODE_2_IP="$3"
+
 # Create or overwrite the custom.ini file
 echo "[master]" > "$OUTPUT_FILE"
-echo "$1 ansible_ssh_private_key_file=/home/runner/work/Main_Project/Main_Project/Terraform/private_key.pem ansible_user=vengalasanjay000" >> "$OUTPUT_FILE"
+echo "$MASTER_IP ansible_ssh_private_key_file=/home/runner/work/Main_Project/Main_Project/Terraform/private_key.pem ansible_user=vengalasanjay000" >> "$OUTPUT_FILE"
 
 # Create or append the nodes section
 echo "[nodes]" >> "$OUTPUT_FILE"
-shift  # Remove the first argument (master IP)
-for node in "$@"; do
-  echo "$node ansible_ssh_private_key_file=/home/runner/work/Main_Project/Main_Project/Terraform/private_key.pem ansible_user=vengalasanjay000" >> "$OUTPUT_FILE"
-done
+echo "$NODE_1_IP ansible_ssh_private_key_file=/home/runner/work/Main_Project/Main_Project/Terraform/private_key.pem ansible_user=vengalasanjay000" >> "$OUTPUT_FILE"
+echo "$NODE_2_IP ansible_ssh_private_key_file=/home/runner/work/Main_Project/Main_Project/Terraform/private_key.pem ansible_user=vengalasanjay000" >> "$OUTPUT_FILE"
