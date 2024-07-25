fafd58b5cb623c522c8498903745b795e0175055
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 12:08:42 +0530

    Update update_custom_ini.sh

--- Terraform/update_custom_ini.sh
+++ Terraform/update_custom_ini.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Logging
+set -x
+
 # Create or overwrite the custom.ini file with the master section
-echo "[master]" > custom.ini
-echo "$1 ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> custom.ini
+echo "[master]" > Terraform/custom.ini
+echo "$1 ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> Terraform/custom.ini
 
 # Create or append the nodes section
-echo "[nodes]" >> custom.ini
+echo "[nodes]" >> Terraform/custom.ini
 shift  # Remove the first argument (master IP)
 for node in "$@"; do
-  echo "$node ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> custom.ini
+  echo "$node ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> Terraform/custom.ini
 done
 
+# Logging
+cat Terraform/custom.ini
+set +x
