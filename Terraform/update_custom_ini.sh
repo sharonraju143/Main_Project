44d06058bf6d579c7ec2ff49181b04bff1802288
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 12:28:14 +0530

    Update update_custom_ini.sh

--- Terraform/update_custom_ini.sh
+++ Terraform/update_custom_ini.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+set -x
+
+# Define absolute path for the Terraform directory
+TERRAFORM_DIR="/home/runner/work/Main_Project/Main_Project/Terraform"
+
 # Create or overwrite the custom.ini file with the master section
-echo "[master]" > Terraform/custom.ini
-echo "$1 ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> Terraform/custom.ini
+echo "[master]" > "$TERRAFORM_DIR/custom.ini"
+echo "$1 ansible_ssh_private_key_file=$TERRAFORM_DIR/private_key.pem ansible_user=vengalasanjay000" >> "$TERRAFORM_DIR/custom.ini"
 
 # Create or append the nodes section
-echo "[nodes]" >> Terraform/custom.ini
+echo "[nodes]" >> "$TERRAFORM_DIR/custom.ini"
 shift  # Remove the first argument (master IP)
 for node in "$@"; do
-  echo "$node ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> Terraform/custom.ini
+  echo "$node ansible_ssh_private_key_file=$TERRAFORM_DIR/private_key.pem ansible_user=vengalasanjay000" >> "$TERRAFORM_DIR/custom.ini"
 done
 
 # Log the contents of custom.ini
-cat Terraform/custom.ini
+cat "$TERRAFORM_DIR/custom.ini"
+
+set +x
