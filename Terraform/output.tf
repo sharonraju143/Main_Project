f8625cc49c8778d789585d1e8a7704154a99f166
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 12:50:55 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -12,7 +12,7 @@ output "node_2_ip" {
 
 resource "null_resource" "update_custom_ini" {
   provisioner "local-exec" {
-    command = "bash -x /home/runner/work/Main_Project/Main_Project/Terraform/update_custom_ini.sh ${google_compute_instance.k8s-master.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-1.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-2.network_interface[0].access_config[0].nat_ip}"
+    command = "shell /home/runner/work/Main_Project/Main_Project/Terraform/update_custom_ini.sh ${google_compute_instance.k8s-master.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-1.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-2.network_interface[0].access_config[0].nat_ip}"
   }
 
   triggers = {
