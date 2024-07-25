1607b902eb1e0aafc9f76c6f51c0c34882275f7e
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 12:17:20 +0530

    Update update_custom_ini.sh

--- Terraform/update_custom_ini.sh
+++ Terraform/update_custom_ini.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Logging
-set -x
-
 # Create or overwrite the custom.ini file with the master section
 echo "[master]" > Terraform/custom.ini
 echo "$1 ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> Terraform/custom.ini
@@ -14,6 +11,5 @@ for node in "$@"; do
   echo "$node ansible_ssh_private_key_file=~/Terraform/private_key.pem ansible_user=vengalasanjay000" >> Terraform/custom.ini
 done
 
-# Logging
+# Log the contents of custom.ini
 cat Terraform/custom.ini
-set +x
