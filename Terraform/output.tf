bfcf8621463f8939348ee1737f7e938eef8961d0
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2024-07-25 12:22:20 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -12,7 +12,13 @@ output "node_2_ip" {
 
 resource "null_resource" "update_custom_ini" {
   provisioner "local-exec" {
-    command = "./update_custom_ini.sh ${google_compute_instance.k8s-master.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-1.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-2.network_interface[0].access_config[0].nat_ip}"
+    command = "bash -x ./update_custom_ini.sh ${google_compute_instance.k8s-master.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-1.network_interface[0].access_config[0].nat_ip} ${google_compute_instance.k8s-node-2.network_interface[0].access_config[0].nat_ip}"
+  }
+
+  triggers = {
+    master_ip = google_compute_instance.k8s-master.network_interface[0].access_config[0].nat_ip
+    node_1_ip = google_compute_instance.k8s-node-1.network_interface[0].access_config[0].nat_ip
+    node_2_ip = google_compute_instance.k8s-node-2.network_interface[0].access_config[0].nat_ip
   }
 
   depends_on = [
@@ -21,3 +27,4 @@ resource "null_resource" "update_custom_ini" {
     google_compute_instance.k8s-node-2
   ]
 }
+
